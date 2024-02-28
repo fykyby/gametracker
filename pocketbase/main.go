b9ca48c3f83193493d30cea0977627f0f9b9c072
fykyby <95149002+fykyby@users.noreply.github.com>
2024-02-28 17:57:03 +0100

    pb tweaks

--- pocketbase/main.go
+++ pocketbase/main.go
@@ -4,10 +4,17 @@ import (
 	"log"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	// uncomment once you have at least one .go migration file in the "migrations" directory
+	// _ "github.com/fykyby/gametracker/migrations"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: false,
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
