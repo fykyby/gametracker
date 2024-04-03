b99430d136f87cf6696c42c48853875ec3831a59
fykyby <95149002+fykyby@users.noreply.github.com>
2024-04-03 02:17:58 +0200

    Decrease spacing between section title and bottom border

--- sveltekit/src/lib/components/GamePageSection.svelte
+++ sveltekit/src/lib/components/GamePageSection.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section>
-	<h2 class="text-xl border-b pb-2 md:pb-3">{title}</h2>
+	<h2 class="text-xl border-b pb-2 md:pb-1.5">{title}</h2>
 	<div class="py-2 md:py-3 flex flex-wrap gap-1 md:gap-2">
 		<slot />
 	</div>
